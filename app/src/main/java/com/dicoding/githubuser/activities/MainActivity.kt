aa001a1f6aff1accf63490f0fef8a7f05d3b597a
Rizky Fadillah <fadillahrizki16@gmail.com>
2023-03-26 22:10:10 +0700

    fix build gradle

--- app/src/main/java/com/dicoding/githubuser/activities/MainActivity.kt
+++ app/src/main/java/com/dicoding/githubuser/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
